fde9a144e8a4deda0e20c9d4cd42c8dfc14a2246
ybe <y.bezci@42-gmbh.de>
2024-06-23 21:29:28 +0200

    inital controller

---
+++ MessageApp/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using MessageApp.Business;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessageApp.Controllers;
+
+public class HomeController
+{
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly MyBusiness _myBusiness;
+        public MessageController(MyBusiness myBusiness)
+        {
+            _myBusiness = myBusiness;
+        }
+
+        [HttpGet("{message}")]
+        public async Task<IActionResult> Index(string message)
+        {
+            await _myBusiness.SendMessageAsync(message);
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsync(string message)
+        {
+            await _myBusiness.SendMessageAsync(message);
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsyncToUser(string user, string message)
+        {
+            await _myBusiness.SendMessageAsyncToUser(user, message);
+            return Ok();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsyncToGroup(string group, string message)
+        {
+            await _myBusiness.SendMessageAsyncToGroup(group, message);
+            return Ok();
+        }
+    }
+}
