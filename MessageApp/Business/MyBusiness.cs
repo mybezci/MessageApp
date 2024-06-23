00fc7337e3dce68e5fdc8ae0926f94d491a7517e
ybe <y.bezci@42-gmbh.de>
2024-06-23 21:28:50 +0200

    inital business

---
+++ MessageApp/Business/MyBusiness.cs
@@ -0,0 +1,29 @@
+﻿using MessageApp.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MessageApp.Business;
+
+public class MyBusiness
+{
+    private readonly IHubContext<MyHub> _hubContext;
+    public MyBusiness(IHubContext<MyHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
+    
+    public async Task SendMessageAsync(string message)
+    {
+        await _hubContext.Clients.All.SendAsync("ReceiveMessage", message);
+    }
+    
+    public async Task SendMessageAsyncToUser(string user, string message)
+    {
+        await _hubContext.Clients.User(user).SendAsync("ReceiveMessage", message);
+    }
+    
+    public async Task SendMessageAsyncToGroup(string group, string message)
+    {
+        await _hubContext.Clients.Group(group).SendAsync("ReceiveMessage", message);
+    }
+
+}
