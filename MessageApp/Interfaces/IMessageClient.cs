7e63f5ba971a3b327415e80f503b4e74b35358ad
ybe <y.bezci@42-gmbh.de>
2024-06-23 21:29:14 +0200

    inital IMessageClient

---
+++ MessageApp/Interfaces/IMessageClient.cs
@@ -0,0 +1,11 @@
+﻿namespace MessageApp.Interfaces;
+
+public interface IMessageClient
+{
+
+    Task Clients(List<string> clients);
+    Task UserJoined(string connectionId);
+    Task UserLeaved(string connectionId);
+    
+
+}
